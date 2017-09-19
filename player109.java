01853a4210bb669b2af2c91f8d1c449b2e3fa62a
Rahiel Kasim <rahielkasim@gmail.com>
2017-09-19 11:52:44 +0200

    code formatting

--- player109.java
+++ player109.java
@@ -4,54 +4,55 @@ import org.vu.contest.ContestEvaluation;
 import java.util.Random;
 import java.util.Properties;
 
+
 public class player109 implements ContestSubmission
 {
-	Random rnd_;
-	ContestEvaluation evaluation_;
+    Random rnd_;
+    ContestEvaluation evaluation_;
     private int evaluations_limit_;
-	
-	public player109()
-	{
-		rnd_ = new Random();
-	}
-	
-	public void setSeed(long seed)
-	{
-		// Set seed of algortihms random process
-		rnd_.setSeed(seed);
-	}
 
-	public void setEvaluation(ContestEvaluation evaluation)
-	{
-		// Set evaluation problem used in the run
-		evaluation_ = evaluation;
-		
-		// Get evaluation properties
-		Properties props = evaluation.getProperties();
+    public player109()
+    {
+        rnd_ = new Random();
+    }
+
+    public void setSeed(long seed)
+    {
+        // Set seed of algortihms random process
+        rnd_.setSeed(seed);
+    }
+
+    public void setEvaluation(ContestEvaluation evaluation)
+    {
+        // Set evaluation problem used in the run
+        evaluation_ = evaluation;
+
+        // Get evaluation properties
+        Properties props = evaluation.getProperties();
         // Get evaluation limit
         evaluations_limit_ = Integer.parseInt(props.getProperty("Evaluations"));
-		// Property keys depend on specific evaluation
-		// E.g. double param = Double.parseDouble(props.getProperty("property_name"));
+        // Property keys depend on specific evaluation
+        // E.g. double param = Double.parseDouble(props.getProperty("property_name"));
         boolean isMultimodal = Boolean.parseBoolean(props.getProperty("Multimodal"));
         boolean hasStructure = Boolean.parseBoolean(props.getProperty("Regular"));
         boolean isSeparable = Boolean.parseBoolean(props.getProperty("Separable"));
 
-		// Do sth with property values, e.g. specify relevant settings of your algorithm
-        if(isMultimodal){
+        // Do sth with property values, e.g. specify relevant settings of your algorithm
+        if (isMultimodal) {
             // Do sth
-        }else{
+        } else {
             // Do sth else
         }
     }
-    
-	public void run()
-	{
-		// Run your algorithm here
-        
+
+    public void run()
+    {
+        // Run your algorithm here
+
         int evals = 0;
         // init population
         // calculate fitness
-        while(evals<evaluations_limit_){
+        while (evals < evaluations_limit_) {
             // Select parents
             // Apply crossover / mutation operators
             double child[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
@@ -61,5 +62,5 @@ public class player109 implements ContestSubmission
             // Select survivors
         }
 
-	}
+    }
 }

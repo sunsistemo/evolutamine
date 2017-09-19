238cbec9a6426a2c9174e0e5862d46f1aeb71d69
Rahiel Kasim <rahielkasim@gmail.com>
2017-09-19 13:37:34 +0200

    population initialization

--- player50.java
+++ player50.java
@@ -1,25 +1,29 @@
-import org.vu.contest.ContestSubmission;
 import org.vu.contest.ContestEvaluation;
+import org.vu.contest.ContestSubmission;
 
-import java.util.Random;
 import java.util.Properties;
+import java.util.Random;
 
 
 public class player50 implements ContestSubmission
 {
-    Random rnd_;
+    Random rnd;
     ContestEvaluation evaluation_;
     private int evaluations_limit_;
+    private Population population;
+    private final int populationSize = 100;
+    String name;
 
     public player50()
     {
-        rnd_ = new Random();
+        name = "evolutamine";
+        rnd = new Random();
     }
 
     public void setSeed(long seed)
     {
         // Set seed of algortihms random process
-        rnd_.setSeed(seed);
+        rnd.setSeed(seed);
     }
 
     public void setEvaluation(ContestEvaluation evaluation)
@@ -48,19 +52,27 @@ public class player50 implements ContestSubmission
     public void run()
     {
         // Run your algorithm here
-
         int evals = 0;
+
         // init population
+        population = initPopulation();
+        population.populate(rnd);
+
         // calculate fitness
         while (evals < evaluations_limit_) {
             // Select parents
             // Apply crossover / mutation operators
             double child[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
-            // Check fitness of unknown fuction
+            // Check fitness of unknown function
             Double fitness = (double) evaluation_.evaluate(child);
             evals++;
             // Select survivors
         }
 
+    }
+
+    private Population initPopulation()
+    {
+        return new Population(populationSize);
     }
 }

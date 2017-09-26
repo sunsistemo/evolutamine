53e321a79dde5a8c2ff79697191e8c389fe9142a
Rahiel Kasim <rahielkasim@gmail.com>
2017-09-26 15:34:29 +0200

    implement uncorrelated mutation with one step size (#7)

--- Individual.java
+++ Individual.java
@@ -6,6 +6,7 @@ public class Individual
 {
     public double[] value;
     public double fitness;
+    public double sigma;
     public double probability;
     public int rank;
     Random rnd;
@@ -18,6 +19,7 @@ public class Individual
         fitness = 0.0;
         probability = 0.0;
         rank = 0;
+        sigma = 0.05;
         rnd = new Random();
     }
 
@@ -28,7 +30,6 @@ public class Individual
 
     public void mutate(double rate, double stepSize)
     {
-        //uniformMutation(rate);
         nonUniformMutation(stepSize);
     }
 
@@ -45,17 +46,36 @@ public class Individual
         }
     }
     
-    // Page 57 of the book. Mutation probability per gene is 1, but sigma controls to which extent
+    // Page 57 of the book. Mutation probability per gene is 1, but sd controls to which extent
     private void nonUniformMutation(double sd)
     {
         for (int i = 0; i < value.length; i++) {
             double h = rnd.nextGaussian() * sd;
-            value[i] += h;
-            if (h < 0) {
-                value[i] = Math.max(value[i], LB);
-            } else if (h > 0) {
-                value[i] = Math.min(value[i], UB);
-            }
-        }    
+            value[i] = boundedAdd(value[i], h);
+        }
+    }
+
+    private void uncorrelatedMutationWithOneStepSize()
+    {
+        double tau = 0.9;
+        double epsilon = 0.025;
+        double gamma = tau * rnd.nextGaussian();
+        sigma *= Math.exp(gamma);
+        sigma = Math.max(sigma, epsilon);
+
+        for (int i = 0; i < value.length; i++) {
+            value[i] = boundedAdd(value[i], sigma * rnd.nextGaussian());
+        }
+    }
+
+    // check so v stays in domain of function
+    private double boundedAdd(double v, double dv)
+    {
+        if (dv < 0) {
+            v = Math.max(v + dv, LB) ;
+        } else if (dv > 0) {
+            v = Math.min(v + dv, UB);
+        }
+        return v;
     }
 }
